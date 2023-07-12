59ad53d8a3a361065449e8bf52c034cf05eefa4c
Pratap Singh <ps6067966@gmail.com>
2023-07-12 17:49:18 +0530

    ci/cd successfull

--- lib/features/index/index_screen.dart
+++ lib/features/index/index_screen.dart
@@ -15,7 +15,7 @@ class _IndexScreenState extends State<IndexScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Welcome everyone, Flash Coders"),
+            Text("Welcome, Flash Coders"),
           ],
         ),
       ),
