57105de5789c6983fb70e522bf6363305e2fd2ad
Pratap Singh <ps6067966@gmail.com>
2023-07-13 15:31:34 +0530

    no page transition animation for web.

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,10 +1,23 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+import 'no_transition_builder.dart';
 
 class AppTheme {
  static ThemeData getTheme() {
     return ThemeData(
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       useMaterial3: true,
+      pageTransitionsTheme: PageTransitionsTheme(
+        builders: kIsWeb
+            ? {
+                for (final platform in TargetPlatform.values)
+                  platform: const NoTransitionsBuilder(),
+              }
+            : const {
+              },
+      ),
     );
+
   }
 }
