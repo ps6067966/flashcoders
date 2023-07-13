a99f4e6c145f7bfb5d8d30cb90a6819df244dc06
Pratap Singh <ps6067966@gmail.com>
2023-07-13 11:44:32 +0530

    introduction docs update.

--- lib/features/documentation/1_getting_started/introduction.dart
+++ lib/features/documentation/1_getting_started/introduction.dart
@@ -1,6 +1,8 @@
 const introductionPath = "/getting-started/introduction";
 const introductionHtml = """
-<h1>Introduction</h1>
-<br>
-<p> Welcome to introduction</p>
+<h1><span style="font-family: Georgia, serif;">Introduction</span></h1>
+<p><span style="font-family: Georgia, serif;">We started this company Flash Coders with the vision <em>to create technology that empowers people to live better lives and make the world a better place, while also being mindful of the potential negative impacts of technology and working to mitigate them, and to do so in a way that is equitable, inclusive, sustainable, future-proof, and aligned with the UN Sustainable Development Goals, and that leaves a positive legacy for future generations.</em><br></span><br><span style="font-family: Georgia, serif;"><strong><u>Commitments that never going to change no matter what -&nbsp;</u></strong></span></p>
+<ol>
+    <li><span style="font-family: Georgia, serif;">Everything we are going to create under flash coders brand will be free of cost to all user, it&apos;s a very tough decision to make, but we believe if we help the world selflessly then universe is going to help you.</span><span style="font-family: Georgia, serif;"><br></span></li>
+</ol>
 """;
