31dd8760996dc00a99bcf6e876db01aa0baddad2
Pratap Singh <ps6067966@gmail.com>
2023-07-13 22:01:53 +0530

    yt.

--- lib/features/documentation/1_getting_started/how_to_contribute.dart
+++ lib/features/documentation/1_getting_started/how_to_contribute.dart
@@ -5,6 +5,8 @@ const howToContributeHtml = """
 <ol>
     <li style="font-family: Georgia, serif;">&nbsp;<span style="font-size: 18px;">Clone website repo from github -<br>&nbsp;&nbsp;</span><a data-fr-linked="true" href="https://github.com/ps6067966/flashcoders.git"><span style="font-size: 18px;">https://github.com/ps6067966/flashcoders.git</span></a><br>&nbsp;</li>
     <li style="font-family: Georgia, serif; font-size: 18px;">Run&nbsp;<strong><em>flutter pub get&nbsp;</em></strong>to fetch project dependecies&nbsp;<br><br></li>
-    <li style="font-family: Georgia, serif; font-size: 18px;"><span style="font-size: 18px;">&nbsp;Have close look at&nbsp;</span></li>
+    <li style="font-family: Georgia, serif; font-size: 18px;"><span style="font-size: 18px;">&nbsp;Have close look at repo folder structure, try to understand existing code.&nbsp;</span></li>
 </ol>
+<p><span style="font-family: Georgia, serif; font-size: 18px;">We have used <a href="https://pub.dev/packages/go_router">go_router</a> for handling routes and deep link, <a href="https://pub.dev/packages/flutter_riverpod">flutter_riverpod</a> as state management.</span><br><br><span style="font-family: Georgia, serif; font-size: 18px;">Here&apos;s is short video to learn about go_router - </span></p>
+<p><iframe width="560" height="300" src="https://www.youtube.com/embed/b6Z885Z46cU" title="YouTube video player" frameborder="0" allowfullscreen=""></iframe></p>
 """;
