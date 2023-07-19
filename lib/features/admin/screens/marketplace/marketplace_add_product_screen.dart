2a891b31328780baa54b91854ef34bb6a31dd52d
Pratap Singh <ps6067966@gmail.com>
2023-07-19 09:16:06 +0530

    dep update, some changes in add marke.

--- lib/features/admin/screens/marketplace/marketplace_add_product_screen.dart
+++ lib/features/admin/screens/marketplace/marketplace_add_product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flashcoders/global_components/custom_app_bar/custom_app_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AddProductInMarketplaceScreen extends StatelessWidget {
@@ -6,10 +7,57 @@ class AddProductInMarketplaceScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(),
-      body: Center(
-        child: Text("Add product in marketplace"),
+    return Scaffold(
+      appBar: const CustomAppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 160.0,
+            right: 160,
+          ),
+          child: Column(
+            children: [
+              const Text(
+                "Add Product In Marketplace",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      
+                    },
+                    child: Card(
+                      child: Container(
+                        width: 500,
+                        height: 500,
+                        decoration: BoxDecoration(
+                          border: Border.all(),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Center(
+                          child: Icon(CupertinoIcons.camera),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 30,
+                  ),
+                  const Column(
+                    children: [],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
