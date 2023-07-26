29418e98b667d1b58a1fc8b4b0bd7b50116004ea
Pratap Singh <ps6067966@gmail.com>
2023-07-26 13:19:44 +0530

    create blog notifier

--- lib/features/blog/create_blog_notifier.dart
+++ lib/features/blog/create_blog_notifier.dart
@@ -1,1 +1,16 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final createBlogNotifierProvider =
+    AsyncNotifierProvider<CreateBlogNotifier, void>(() {
+  return CreateBlogNotifier();
+});
+
+class CreateBlogNotifier extends AsyncNotifier<void> {
+
+  publishBlog() {
+    state = const AsyncValue.loading();
+  }
+
+  @override
+  void build() {}
+}
