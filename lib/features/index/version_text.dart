b0bf3de0255049779df3b3a434ed11ae72f0c2dc
Pratap Singh <ps6067966@gmail.com>
2023-11-24 17:37:05 +0530

    dep update

--- lib/features/index/version_text.dart
+++ lib/features/index/version_text.dart
@@ -8,7 +8,7 @@ class VersionText extends StatelessWidget {
     return const Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
-        Text("Version 1.0.0"),
+        Text("Version 1.0.1"),
         SizedBox(
           width: 16,
         ),
